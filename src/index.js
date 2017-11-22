068962f730ca0ed50390bcf34684152333bd60ce
joshwestbury <joshwestbury@gmail.com>
2017-11-22 15:05:27 -0600

    created initial jsx

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from "./components/StorePicker";
+// import './css/style.css';
+
+
+render(<StorePicker/>, document.querySelector('#main'));
+
+
+// import StorePicker from './components/StorePicker';
+//
+// render(<StorePicker/>, document.querySelector('#main'));
