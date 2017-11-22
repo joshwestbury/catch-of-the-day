469e276bb66601c74c4d19435de5be0712948d42
joshwestbury <joshwestbury@gmail.com>
2017-11-22 16:03:20 -0600

    passed some dynamic data with 'props'

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
         return (
             <div className="catch-of-the-day">
                 <div class="menu">
-                    <Header />
+                    <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order />
                 <Inventory />
