6d2d317dabd17c3b21937364b8185f3a543d6a63
joshwestbury <joshwestbury@gmail.com>
2017-11-24 09:34:43 -0600

    added helper and utility functions

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,7 @@
 import React from 'react';
+//getFunName is what's called a "named import".
+//note that the two dots before /helpers is because the helpers.js file is two levels up in the file tree.
+import {getFunName} from '../helpers.js';
 
 
 class StorePicker extends React.Component {
@@ -6,7 +9,7 @@ class StorePicker extends React.Component {
         return (
             <form className="store-selector">
                 <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name" />
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit">Visit Store →</button>
             </form>
         )
